80612951040a96b16329335b1af4639d52ca6540
mnybe1 <marion.nyberg1@gmail.com>
2020-03-06 17:57:22 -0800

    download and save data to data folder

--- load_data.R
+++ load_data.R
@@ -1,3 +1,5 @@
 data_url <- read.csv(url('https://raw.githubusercontent.com/STAT547-UBC-2019-20/group_2_youtube/master/data/CAvideos.csv'))
 
+write.csv(data_url, here::here("data","Youtube_data.csv"))
+
 print('Data has been downloaded successfully')
