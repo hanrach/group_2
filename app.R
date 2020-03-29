406286a0c310a948f4d3dcce7e56ced19c8c5491
Rachel Han <hanrach@Rachels-MBP-2.hitronhub.home>
2020-03-28 21:39:21 -0700

    added bar plot and 2 scatter plots to app.R

--- app.R
+++ app.R
@@ -13,25 +13,101 @@ library(plotly)
 library(purrr)
 library(tidyverse)
 
-
 app <- Dash$new()
 
-heading = htmlH1('Canadian YouTube Statistics')
-heading_subtitle = htmlH3('Exploring the relationship between YouTube video categories, view number, comment count, and likes and dislike numbers
+# load data
+CAN <- read.csv("data/youtube_processed.csv")
+
+# make graphs
+bar_plot <- function(){
+  category_vids <- CAN %>% group_by(category_id) %>% 
+    tally() %>% 
+    arrange(desc(n))
+  
+  p<-category_vids %>% ggplot(aes(y=n,
+                                   x = fct_reorder(as.factor(category_id),
+                                                   n,
+                                                   max, .incr=TRUE))) +
+    geom_bar(stat="identity") + 
+    coord_flip() + 
+    ylab("count") + 
+    xlab("category") + 
+    theme_bw() +
+    theme(legend.position = "none") +
+    ggtitle("Number of videos by Category")
+  
+  ggplotly(p, tooltip = c("text"))
+}
+
+views_scatter <- function(category = 24){
+  data <- CAN %>% filter(category_id == category)
+  p <- ggplot(data, aes(y=views)) +
+    geom_point(aes(x=likes, color = "likes"),alpha =0.2,position="jitter") + 
+    geom_point(aes(x = dislikes, color = "dislikes"), alpha =0.2,position="jitter") + 
+    scale_x_continuous(labels = scales::comma_format()) +
+    scale_y_continuous(labels = scales::comma_format()) +
+    labs(x = "Count of likes/dislikes", y = "Views") +
+    ggtitle("Trends between likes/dislikes and views")+
+    theme_bw()
+    
+    ggplotly(p, tooltip = c("text"))
+}
+
+comments_scatter <- function(category = 24){
+  data <- CAN %>% filter(category_id == category)
+  p <- ggplot(data, aes(y=comment_count)) +
+    geom_point(aes(x=likes, color = "likes"),alpha =0.2,position="jitter") + 
+    geom_point(aes(x = dislikes, color = "dislikes"), alpha =0.2,position="jitter") + 
+    scale_x_continuous(labels = scales::comma_format()) +
+    scale_y_continuous(labels = scales::comma_format()) +
+    labs(x = "Count of likes/dislikes", y = "Comments") +
+    ggtitle("Trends between likes/dislikes and comments")+
+    theme_bw()
+  
+  ggplotly(p, tooltip = c("text"))
+}
+
+barplot <- dccGraph(
+  id = 'barplot',
+  figure = bar_plot()
+)
+
+views_scatterplot <- dccGraph(
+  id = 'views_scatterplot',
+  figure = views_scatter()
+)
+
+comments_scatterplot <- dccGraph(
+  id = 'comments_scatterplot',
+  figure = comments_scatter()
+)
+
+# layout set up
+div_header <- htmlDiv(
+  list(
+    htmlH1("Canadian YouTube Statistics"),
+    htmlH3('Exploring the relationship between YouTube video categories, view number, comment count, and likes and dislike numbers
 ')
+  ), style = list('columnCount'=1, 
+                  'background-color'= 'lightgrey', 
+                  'color'='white')
+)
 
-
-
-
+div_main <- htmlDiv(
+  list(
+    barplot,
+    views_scatterplot,
+    comments_scatterplot
+  )
+)
 
 app$layout(
+	div_header,
 	htmlDiv(
-		list(
-			heading,
-			heading_subtitle
-	#ADD MORE COMPONENTS HERE	
-		)
+	  list(
+	    div_main
+	  )
 	)
 )
 
-app$run_server(debug=TRUE)
+app$run_server(host='0.0.0.0',debug=TRUE)
