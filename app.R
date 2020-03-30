b39dbe4ea6db938fbe41f1d2bb0dd7ff0d154a5d
mnybe1 <marion.nyberg1@gmail.com>
2020-03-30 16:54:11 -0700

    add layout

--- app.R
+++ app.R
@@ -13,8 +13,12 @@ library(ggplot2)
 library(plotly)
 library(purrr)
 library(tidyverse)
+library(flexdashboard)
 
 app <- Dash$new()
+
+#set plot dimensions
+options(repr.plot.width = 10, repr.plot.height = 10)
 
 # load data
 CAN <- read.csv("data/youtube_processed.csv")
@@ -22,23 +26,6 @@ CAN <- read.csv("data/youtube_processed.csv")
 #make range sliders
 max_status_count = max(max(CAN$likes),max(CAN$dislikes))
 min_status_count = 0
-#views_slider <- dccRangeSlider(
-#	id = 'views-slider',
-#	min = 0,
-#	max = max_status_count,
-#	step = 100,
-	# marks = setNames(as.list(paste(seq(0, max_status_count, 1000000))),
-	#                  paste(seq(0, max_status_count, 1000000))),
-#	marks = list(
-#	  "0" = "0",
-#	  "1000000" = list(label = "1,000,000"), 
-#	  "2000000" = list(label = "2,000,000"),
-#	  "3000000" = list(label = "3,000,000"),
-#	  "4000000" = list(label = "4,000,000"),
-#	  "5000000" = list(label = "5,000,000")
-#	),
-#	value = list(min_status_count, max_status_count)
-#)
 
 comments_slider <- dccSlider(
 	id = 'comments-slider',
@@ -62,10 +49,11 @@ histogram_plot <- function(category = 24) {
     mutate(year_month = format(as.Date(trending_date), "%Y-%m") ) %>%
     arrange(trending_date) %>%
     ggplot(aes(x=as.Date(trending_date),y=views)) + geom_bar(stat = "identity") +
-    labs(x = "Date", y = "Number of views") +
-    ggtitle(paste0("View counts over time in entertainment for category", paste(category))) +
+    labs(x = "Date", y = "View number") +
+    ggtitle(paste0("View Counts Over Time in Entertainment for Category ", paste(category))) +
     scale_y_continuous(labels = scales::comma_format()) +
-    scale_x_date(date_breaks = "months")
+    scale_x_date(date_breaks = "months")+
+  	theme_bw()
   
   ggplotly(p)
 }
@@ -82,32 +70,14 @@ bar_plot <- function(){
                                   customdata=category_id)) +
     geom_bar(stat="identity") + 
     coord_flip() + 
-    ylab("count") + 
-    xlab("category") + 
+    ylab("Count") + 
+    xlab("Category") + 
     theme_bw() +
     theme(legend.position = "none") +
-    ggtitle("Number of videos by Category")
+    ggtitle("Number of Videos by Category")
   
   ggplotly(p) %>% layout(clickmode = 'event+select')
 }
-
-#views_scatter <- function(category_select = 24, likes_interval = list(0, max_status_count)){
-  
-  #data <- CAN %>% filter(category_id == category_select) %>% 
-    #filter(likes <= likes_interval[[2]][1] & dislikes <= likes_interval[[2]][1]) %>%
-    #filter(likes >= likes_interval[[1]][1] & dislikes >= likes_interval[[1]][1])
-  
-  #p <- ggplot(data, aes(y=views)) +
-    #geom_point(aes(x=likes, color = "likes"),alpha =0.2,position="jitter") + 
-    #geom_point(aes(x = dislikes, color = "dislikes"), alpha =0.2,position="jitter") + 
-    #scale_x_continuous(labels = scales::comma_format()) +
-    #scale_y_continuous(labels = scales::comma_format()) +
-    #labs(x = "Count of likes/dislikes", y = "Views") +
-    #ggtitle(paste0("Trends between likes/dislikes and views for category ", toString(category_select))) +
-    #theme_bw()
-    
-    #ggplotly(p)
-#}
 
 comments_scatter <- function(category_select = 24, likes_max = max_status_count, yaxis = "views") { ### original comments_scatter <- function(category_select = 24, likes_max = max_status_count) {
   y_label <- yaxisKey$label[yaxisKey$value == yaxis]
@@ -120,18 +90,17 @@ comments_scatter <- function(category_select = 24, likes_max = max_status_count,
     scale_y_continuous(labels = scales::comma_format()) +
   	xlab("Count of likes/dislikes") +
   	ylab(y_label)+
-    #labs(x = "Count of likes/dislikes", y = "Comments") 
-    ggtitle(paste0("Trends between likes/dislikes and ", y_label, " for category ", toString(category_select)))+ ###### original gtitle(paste0("Trends between likes/dislikes and comments for category ", toString(category_select)))
+    ggtitle(paste0("Trends Between Likes/Dislikes and ", y_label, " for Category ", toString(category_select)))+ ###### original gtitle(paste0("Trends between likes/dislikes and comments for category ", toString(category_select)))
     theme_bw()
   
   ggplotly(p, tooltip = c("text"))
 }
 
 
-###### Storing the labels/values as a tibble means we can use this both 
-# to create the dropdown and convert colnames -> labels when plotting
+# Storing the labels/values as a tibble means we can use this both to create the dropdown and convert colnames -> labels when plotting
 yaxisKey <- tibble(label = c("View number", "Comment count"),
 									 value = c("views", "comment_count"))
+
 #Create the dropdown
 yaxisDropdown <- dccDropdown(
 	id = "y-axis",
@@ -141,29 +110,27 @@ yaxisDropdown <- dccDropdown(
 		}),
 	value = "views"
 )
-#######
 
 barplot <- dccGraph(
   id = 'barplot',
-  figure = bar_plot()
+  figure = bar_plot(),
+  config = list('displaylogo' = FALSE)
 )
 
 histogram <- dccGraph(
   id = 'histogram',
-  figure = histogram_plot()
+  figure = histogram_plot(),
+  config = list('displaylogo' = FALSE)
 )
-
-#views_scatterplot <- dccGraph(
-  #id = 'views_scatterplot',
-  #figure = views_scatter()
-#)
 
 comments_scatterplot <- dccGraph(
   id = 'comments_scatterplot',
-  figure = comments_scatter()
+  figure = comments_scatter(),
+  config = list('displaylogo' = FALSE)
 )
 
 # layout set up
+#this is the title bar
 div_header <- htmlDiv(
   list(
     htmlH1("Canadian YouTube Statistics"),
@@ -171,61 +138,75 @@ div_header <- htmlDiv(
 ')
   ), style = list('columnCount'=1, 
                   'background-color'= 'lightgrey', 
-                  'color'='white')
+                  'color'='white',
+  								'text-align'='center')
 )
 
+
+
+#this is the sidebar
+div_side <- htmlDiv(
+	list(
+		htmlLabel('Select scatter plot to display view number or comment count:'),
+		htmlBr(),
+		yaxisDropdown,
+		htmlLabel('Change range of like/dislike values:'),
+		htmlBr(),
+		comments_slider
+	),
+	style = list('background-color'='lightgrey', 
+							 'columnCount'=1, 
+							 'white-space'='pre-line',
+							 'width' = '60%')
+)
+
+
+#barplot div
+div_barplot <- htmlDiv(
+	list(
+		barplot
+	),
+	style = list('display' = 'flex',
+							 'height' = '40%')
+)
+
+#this is the main element
 div_main <- htmlDiv(
   list(
-    barplot,
     histogram,
-    #views_scatterplot,
-    #views_slider,
-    comments_scatterplot,
-    comments_slider,
-    yaxisDropdown
-  )
+    htmlBr(),
+    comments_scatterplot
+  ),
+  style = list('display' = 'flex',
+  						 'flex-direction'= 'column',
+  						 'width' = '50%')
 )
 
 
+#final layout
 app$layout(
 	div_header,
 	htmlDiv(
-	  list(
+		list(
+			div_side
+		),
+		style = list('display' = 'flex',
+			'justify-content' = 'center')
+	),
+	htmlDiv(
+		list(
+	  	div_barplot,
 	  	div_main
-	  		)
-	  	)
+	  		),
+		style = list('display' = 'flex',
+								 'flex-direction' = 'row',
+								 'align-items' = 'baseline')
 	  )
+)
+
 	
 # add callbacks
-#to update views scatter plot based on rangeslider
-# app$callback(
-# 	output = list(id = 'views_scatterplot', property = 'figure'),
-# 	params = list(input(id = 'views-slider', property = 'value')),
-# 	function(likes_max) {
-# 		views_scatter(24, likes_max)
-# 	})
-
-# to update comments scatter plot based on rangeslider
-# app$callback(
-# 	output = list(id = 'comments_scatterplot', property = 'figure'),
-# 	params = list(input(id = 'comments-slider', property = 'value')),
-# 	function(likes_max) {
-# 		comments_scatter(24, likes_max)
-# 	})
-
-#to update views scatter plot by clicking on barplot and slider
-#app$callback(output = list(id = 'views_scatterplot', property = 'figure'),
-#             params = list(input(id = 'barplot', property='clickData'),
-#                           #input(id='views-slider', property='value')
-#             ),
-#             function(clickData, interval) {
-#               category = clickData$points[[1]]$customdata
-#               if (is.null(category)) {
-#                 category <- 24
-#               }
-#               #views_scatter(category, interval)
-#             })
-
+#to update views historgram plot from clicking on barplot
 app$callback(output = list(id = 'histogram', property = 'figure'),
              params = list(input(id = 'barplot', property='clickData')
              ),
